a1a0d74917969145360e59543ea047d7e415f718
Rmadhu21 <madh20121.cb@rmkec.ac.in>
2021-05-30 18:52:54 +0000

    modified index and script and style sheet

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    
+    var principal = document.getElementById("principal").value;
+    if (principal > 0){
+    var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+      var result = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerText=result;
+        document.getElementById("year").innerText=year;
+    }
+    else {
+        alert("Enter a positive number.")
+    }    
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
